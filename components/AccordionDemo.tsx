8e98274a80a5af893cb84d4cdc42d2b7a35fd16f
mbialekIBPMPRO <mbialek@ibpm.pro>
2023-09-22 15:59:28 +0200

    update apos

--- components/AccordionDemo.tsx
+++ components/AccordionDemo.tsx
@@ -17,22 +17,16 @@ import {
         <AccordionItem value="item-2">
           <AccordionTrigger>MQ</AccordionTrigger>
           <AccordionContent>
-            Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionTrigger>REST AIP</AccordionTrigger>
           <AccordionContent>
-            Yes. It&apos;s animated by default, but you can disable it if you
-            prefer.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
           <AccordionTrigger>HTTP/S</AccordionTrigger>
           <AccordionContent>
-            Yes. It&apos;s animated by default, but you can disable it if you
-            prefer.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
--- components/about.tsx
+++ components/about.tsx
@@ -3,9 +3,9 @@ import AccordionDemo from "./AccordionDemo";
 const About = () => {
     return (<>
         <section className="mt-16">
-            <h1 className="text-7xl font-bold">HI I'm <span className="text-purple-600">Michał</span></h1>
-            <h3 className="text-4xl my-3">I'm IIB Developer</h3>
-            <p className="text-gray-600 mb-8">Currently, I'm working with IBM Integration Bus (IIB) and IBM App Connect Enterprise Toolkit,
+            <h1 className="text-7xl font-bold">HI I&apos;m <span className="text-purple-600">Michał</span></h1>
+            <h3 className="text-4xl my-3">I&apos;m IIB Developer</h3>
+            <p className="text-gray-600 mb-8">Currently, I&apos;m working with IBM Integration Bus (IIB) and IBM App Connect Enterprise Toolkit,
                 <br />focusing on the creation and maintenance of integrated systems using REST API, SOAP, YAML configuration files, and XML.
             </p>
         </section>
